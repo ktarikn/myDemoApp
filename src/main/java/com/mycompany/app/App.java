95a12121773746e9987f412a13a992426234b459
Kemal Tarık Nehrozoğlu <tarikn2003@gmail.com>
2024-03-10 18:51:43 +0300

    some message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -44,7 +44,7 @@ public class App
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
 
-        get("/", (req, res) -> "Hello, World");
+        get("/", (req, res) -> "Hello, World123");
 
         post("/compute", (req, res) -> {
           //System.out.println(req.queryParams("input1"));
